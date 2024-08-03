829d5a519eb3c7d81476358928b77c2db8b65e6f
xxziiko <xxziiko@gmail.com>
2024-08-04 01:09:51 +0900

    chore: @stylexjs plugin, react.svg 삭제

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,9 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended",
 		"plugin:react-hooks/recommended",
 	],
-	ignorePatterns: ["dist", ".eslintrc.cjs", "@stylexjs"],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
 	parser: "@typescript-eslint/parser",
-	plugins: ["react-refresh"],
+	plugins: ["react-refresh", "@stylexjs"],
 	rules: {
 		"@stylexjs/valid-styles": "error",
 		"react-refresh/only-export-components": [
