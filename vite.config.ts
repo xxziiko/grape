7f21b79f264b6f7244deeae0b7e99978cce9a1d5
xxziiko <xxziiko@gmail.com>
2024-08-03 22:17:43 +0900

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import styleX from "vite-plugin-stylex";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
