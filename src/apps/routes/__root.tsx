112e1552ca1cb4d2a2b19dfea18537ec08e524c9
xxziiko <xxziiko@gmail.com>
2024-08-08 12:24:48 +0900

    build: 에러 해결

--- src/apps/routes/__root.tsx
+++ src/apps/routes/__root.tsx
@@ -1,11 +1,11 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 export const Route = createRootRoute({
   component: () => (
-    <AuthProvider>
+    <>
       <Outlet />
       {import.meta.env.MODE === 'development' && <TanStackRouterDevtools />}
-    </AuthProvider>
+    </>
   ),
 });
