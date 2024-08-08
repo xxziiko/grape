17a2b118b1ccf48ebe2a7d8c555828c4001515b0
xxziiko <>
2024-08-08 03:19:35 +0000

    build: Babel 및 플러그인 설정 추가하여 stylex 지원

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import babel from "@rollup/plugin-babel";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -5,6 +6,31 @@ import styleX from "vite-plugin-stylex";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react(), TanStackRouterVite(), tsconfigPaths(), styleX()],
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [
+			react(),
+			TanStackRouterVite(),
+			tsconfigPaths(),
+			styleX(),
+			babel({
+				babelHelpers: "bundled",
+				extensions: [".js", ".jsx", ".ts", ".tsx"],
+				presets: ["@babel/preset-react"],
+				plugins: [
+					[
+						"@stylexjs/babel-plugin",
+						{
+							dev: mode !== "production",
+							test: false,
+							unstable_moduleResolution: {
+								type: "commonJS",
+								rootDir: __dirname,
+							},
+						},
+					],
+				],
+			}),
+		],
+	};
 });
