6d0fe8f84ff8a08137c1cf615926fed9cb28d300
xxziiko <xxziiko@gmail.com>
2024-08-27 18:42:01 +0900

    fix: 리다이렉션 주소 추가

--- src/pages/home/api/signInWithKakao.tsx
+++ src/pages/home/api/signInWithKakao.tsx
@@ -2,7 +2,10 @@ import { handleError, supabase } from '@/shared';
 
 const signInWithKakao = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
-    provider: 'kakao'
+    provider: 'kakao',
+    options: {
+      redirectTo: `${import.meta.env.VITE_BASE_URL}/chat`,
+    },
   });
 
   handleError(error);
