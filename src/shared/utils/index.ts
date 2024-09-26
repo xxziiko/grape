790c77c1b0e54575542feb66ecf0f6244789019c
xxziiko <xxziiko@gmail.com>
2024-09-27 02:50:30 +0900

    style: tap 간격 변경

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -1,14 +1,20 @@
-export const handleError = <T>({data, error} : {data: T, error:unknown }) => {
-	if (error) throw error;
-	
-	return data
+export const handleError = <T>({
+  data,
+  error,
+}: {
+  data: T;
+  error: unknown;
+}) => {
+  if (error) throw error;
+
+  return data;
 };
 
 export const assert: (
-	condition: unknown,
-	message: string,
+  condition: unknown,
+  message: string,
 ) => asserts condition = (condition: unknown, message: string) => {
-	if (!condition) {
-		throw new Error(message);
-	}
+  if (!condition) {
+    throw new Error(message);
+  }
 };
