8b64eee13a0f7d04c6819a6a4a75e0e35349528e
xxziiko <xxziiko@gmail.com>
2024-09-27 02:45:59 +0900

    rename: 폴더명 변경

--- src/entities/auth/api/getSession.tsx
+++
@@ -1,8 +0,0 @@
-import { supabase } from '@/shared/api';
-
-const getSession = async () => {
-  const { data } = await supabase.auth.getSession();
-  return data.session;
-};
-
-export default getSession;
--- src/entities/auth/api/signOutUser.tsx
+++
@@ -1,7 +0,0 @@
-import { supabase } from '@/shared/api';
-
-const signOutUser = async () => {
-  await supabase.auth.signOut();
-};
-
-export default signOutUser;
