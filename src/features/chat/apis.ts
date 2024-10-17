c066f19a6d5bd69790ca079b8af3febed8f732c7
xxziiko <xxziiko@gmail.com>
2024-10-18 01:58:24 +0900

    fix: 비동기 함수에 누락된 await 추가

--- src/features/chat/apis.ts
+++ src/features/chat/apis.ts
@@ -9,21 +9,21 @@ export const fetchMessagesForChat = async (chatId: string | undefined) =>
     .then(handleError);
 
 export const fetchUserChats = async (userId: string | undefined) =>
-  supabase
+  await supabase
     .from('chat_list_view')
     .select('*')
     .eq('user_id', userId)
     .then(handleError);
 
 export const fetchUserFriends = async (userId: string | undefined) =>
-  supabase
+  await supabase
     .from('friends')
     .select('friend_id, friend_name')
     .eq('user_id', userId)
     .then(handleError);
 
 export const sendChatMessage = async ({ chat_id, user_id, body }: Messages) =>
-  supabase
+  await supabase
     .from('messages')
     .insert([{ chat_id, user_id, body }])
     .then(handleError);
