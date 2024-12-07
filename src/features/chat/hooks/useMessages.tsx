b6634f461fc29287db6dd8ae0367d52f8998e61d
xxziiko <xxziiko@gmail.com>
2024-12-07 13:23:40 +0900

    refactor: 리얼타임 메세지 debounce 적용

--- src/features/chat/hooks/useMessages.tsx
+++ src/features/chat/hooks/useMessages.tsx
@@ -1,11 +1,11 @@
 import { type Messages, useMessagesQuery } from '@/features/chat';
 import useRealTimeMessages from '@/features/chat/hooks/useRealTimeMessages';
 import { useEffect, useState } from 'react';
+import { debounce } from 'es-toolkit';
 
 const useMessages = (chatId: string | undefined) => {
   const [messages, setMessages] = useState<Messages[]>([]);
   const [realtimeMessages, setRealtimeMessages] = useState<Messages[]>([]);
-  const [realtimeCount, setRealtimeCount] = useState(0);
 
   const {
     data,
@@ -14,20 +14,23 @@ const useMessages = (chatId: string | undefined) => {
     hasNextPage,
     isFetchingNextPage,
     refetch,
-  } = useMessagesQuery(chatId); // realtimeCount 제거
+  } = useMessagesQuery(chatId);
+
+  const updateMessages = debounce((newMessages: Messages[]) => {
+    setRealtimeMessages((prev) => [...prev, ...newMessages]);
+  }, 300);
 
   useRealTimeMessages(chatId, (newMessage) => {
-    setRealtimeMessages((prev) => [...prev, newMessage]);
-    setRealtimeCount((prev) => prev + 1);
+    updateMessages([newMessage]);
   });
 
   useEffect(() => {
     if (!data) return;
 
-    // 메시지 정렬 및 중복 제거
     const allMessages = [...data, ...realtimeMessages].sort(
       (a, b) =>
-        new Date(a.created_at).getTime() - new Date(b.created_at).getTime(),
+        new Date(a.created_at ?? '0').getTime() -
+        new Date(b.created_at ?? '0').getTime(),
     );
 
     const uniqueMessages = Array.from(
@@ -43,7 +46,6 @@ const useMessages = (chatId: string | undefined) => {
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
-    realtimeCount,
     refetch,
   };
 };
